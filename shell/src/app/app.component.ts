697ade16f8da23910f4f72ebc0034ee7356a9ffb
Luis Fernando <luisfernandodass@gmail.com>
2022-12-18 17:22:01 -0300

    feat: ajuste no shell

--- shell/src/app/app.component.ts
+++ shell/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'shell';
+
+  ngOnInit(): void {
+    const script = document.createElement('script');
+    script.src = 'http://127.0.0.1:8080/dist/mfe-amount-of-water-drunk/main.js';
+    document.body.appendChild(script);
+
+    const script2 = document.createElement('script');
+    script2.src = 'http://127.0.0.1:8080/dist/mfe-amount-of-water-drunk/main.js';
+    document.body.appendChild(script2);
+  }
 
   selectMfe(event: Event): void {
     (event.currentTarget as HTMLElement).classList.toggle('btn-active-mfe');
--- shell/src/app/app.module.ts
+++ shell/src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { NavComponent } from './nav/nav.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
